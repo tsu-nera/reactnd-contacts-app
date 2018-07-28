4f572ad4ab8de0449621f7c69e240bc08e6a79d9
Tsunemichi Harada <fox10225fox@gmail.com>
2018-07-28 22:04:26 +0900

    Add default contacts and the ListContacts component

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": ["airbnb", "prettier"],
+    "plugins": ["prettier"],
+    "rules": {
+        "prettier/prettier": "error",
+        "react/prop-types": 0,
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/jsx-one-expression-per-line": 0,
+        "react/prefer-stateless-function" : 0,
+    }   
+};
---
+++ src/ContactLists.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class ListContacts extends Component {
+  render() {
+    return <ol className="contact-list" />
+  }
+}
+
+export default ListContacts
